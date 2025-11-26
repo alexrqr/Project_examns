e43c27a27c19fe78cb4f5d0528268c219898e483
alexrqr <jeanalexromero200@gmail.com>
2025-11-26 05:35:40 -0500

    category controller and exam controller

--- sistema-examenes-backend/src/main/java/com/sistema/examenes/Controllers/CategoryController.java
+++ sistema-examenes-backend/src/main/java/com/sistema/examenes/Controllers/CategoryController.java
@@ -1,10 +1,15 @@
 package com.sistema.examenes.Controllers;
 
 import com.sistema.examenes.Entity.Category;
+import com.sistema.examenes.Payload.ApiResponse;
 import com.sistema.examenes.Repository.CategoryRepository;
+import com.sistema.examenes.Services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/categoria")
@@ -14,12 +19,115 @@ public class CategoryController {
     @Autowired
     private CategoryRepository categoryRepository;
 
-    @PostMapping("/add")
-    public ResponseEntity<Category> guardarCategoria(@RequestBody Category category) {
-        Category newCategory = categoryRepository.save(category);
+    @Autowired
+    private CategoryService categoryService;
 
-        return ResponseEntity.ok().body(newCategory);
+    /**
+     * Guardado de registro
+     * @param category
+     * @return
+     */
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse<Category>> guardarCategoria(@RequestBody Category category) {
+        try{
+            Category newCategory = categoryService.addCategory(category);
+
+            return ResponseEntity.ok(
+                    new ApiResponse<>("success", "Categorúa registrada correctamente", newCategory, null)
+            );
+
+        } catch (Exception e){
+            return ResponseEntity.internalServerError().body(
+                    new ApiResponse<>("error", "No se pudo registrar la categoría", null, e.getMessage())
+            );
+        }
     }
+
+    /**
+     * Búsqueda de por ID
+     * @param categoryId
+     * @return
+     */
+    @GetMapping("/findById/{categoryId}")
+    public ResponseEntity<ApiResponse<Category>> listCategoryById(@PathVariable("categoryId") Long categoryId){
+        try {
+            Category catFinded = categoryService.findById(categoryId);
+
+            return ResponseEntity.ok(
+                    new ApiResponse<>("success", "Categoría encontrada", catFinded, null)
+            );
+
+        } catch (Exception e){
+            return ResponseEntity.internalServerError().body(
+                    new ApiResponse<>("error", "No se pudo encontrar la categoría", null, e.getMessage())
+            );
+        }
+    }
+
+    /**
+     * List de categorías
+     * @return
+     */
+    @GetMapping("/listExamns")
+    public ResponseEntity<ApiResponse<Set<Category>>> listCategorys() {
+        try {
+            Set<Category> listCategorys = categoryService.listAllCategories();
+
+            return ResponseEntity.ok(
+                    new ApiResponse<>("success", "Categorías listadas", listCategorys, null)
+            );
+
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(
+                    new ApiResponse<>("error", "No se pudo listar categorías", null, e.getMessage())
+            );
+        }
+    }
+
+
+    /**
+     * Actualización de categoría
+     * @param category
+     * @return
+     */
+    @PutMapping("/update")
+    public ResponseEntity<ApiResponse<Category>> updatedCategory(@RequestBody Category category) {
+        try {
+            Category updated = categoryService.updateCategory(category);
+
+            return ResponseEntity.ok(
+                    new ApiResponse<>("success", "Categoría actualizada", updated, null)
+            );
+
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(
+                    new ApiResponse<>("error", "No se pudo actualizar la categoría", null, e.getMessage())
+            );
+        }
+    }
+
+
+    /**
+     * Delete categoría
+     * @param categoryId
+     * @return
+     */
+    @DeleteMapping("/delete/{categoryId}")
+    public ResponseEntity<ApiResponse<Void>> deleteCategory(@PathVariable Long categoryId) {
+        try {
+            categoryService.deleteCategory(categoryId);
+
+            return ResponseEntity.ok(
+                    new ApiResponse<>("success", "Categoría eliminada", null, null)
+            );
+
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(
+                    new ApiResponse<>("error", "No se pudo eliminar la categoría", null, e.getMessage())
+            );
+        }
+    }
+
 
 
 }
